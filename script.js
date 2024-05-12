1af37c7ea07cf0bb296fb2709bef8ca0a4cecdbe
Daniel Meneghetti <138326725+DanielMeneRossi@users.noreply.github.com>
2024-05-12 11:36:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function converter(){
+function converter() {
 	var input = document.getElementById('txtn')
-	var numero = Number.parseFloat(input.value)
-	var dolar = numero * 5.1568
-	Number(dolar)
+	var numero = Number(input.value)
 	var res = document.getElementById('res')
-	res.innerHTML = (`Em dólares é: ${dolar.toFixed(2)}`)
+	var tipo = document.getElementsByName('tipo')
+	if (numero == '') {
+		res.innerHTML = ("Coloque um número!")
+	} else {
+		if (tipo[0].checked) {
+			var dolar = numero * 5.1568
+			res.innerHTML = (`Em dólares é: ${dolar.toLocaleString('pt-BR',{style:'currency', currency: 'USD'})}`)
+		} else if (tipo[1].checked) {
+			var euro = numero * 5.562898000000001
+			res.innerHTML = (`Em euros é: ${euro.toLocaleString('pt-BR',{style:'currency', currency: 'EUR'})}`)
+		}
+	}
 }
